2a2dee3c2f234d4e7abeda6b8c6201d3e4c6a15c
Icebob <mereg.norbert@gmail.com>
2019-11-25 22:53:28 +0100

    implementing adapter

---
+++ examples/checker.js
@@ -0,0 +1,66 @@
+/*
+ * moleculer-db
+ * Copyright (c) 2019 MoleculerJS (https://github.com/moleculerjs/moleculer-db)
+ * MIT Licensed
+ */
+
+"use strict";
+
+const _ = require("lodash");
+const Promise = require("bluebird");
+const kleur = require("kleur");
+
+class ModuleChecker {
+
+	constructor(okCount) {
+		this.tests = [];
+		this.okCount = okCount;
+		this.ok = 0;
+		this.fail = 0;
+	}
+
+	add(title, fn, cb) {
+		this.tests.push(() => Promise.resolve(this.printTitle(title)).then(() => fn()).then(rsp => {
+			let res = cb(rsp);
+			if (Array.isArray(res))
+				res.map(r => this.checkValid(r));
+			else if (res != null)
+				this.checkValid(res);
+		}).catch(err => {
+			console.error(kleur.red().bold(err.name, err.message));
+			console.error(err);
+		}));
+	}
+
+	execute() {
+		return Promise.each(this.tests, fn => fn());
+	}
+
+	printTitle(text) {
+		console.log();
+		console.log(kleur.yellow().bold(`--- ${text} ---`));
+	}
+
+	checkValid(cond) {
+		let res = cond;
+		if (_.isFunction(cond))
+			res = cond();
+
+		if (res) {
+			this.ok++;
+			console.log(kleur.bgGreen().yellow().bold("--- OK ---"));
+		} else {
+			this.fail++;
+			console.log(kleur.bgRed().yellow().bold("!!! FAIL !!!"));
+		}
+	}
+
+	printTotal() {
+		console.log();
+		console.log(kleur.bgGreen().yellow().bold(`--- OK: ${this.ok} of ${this.okCount} ---`), this.fail > 0 ? " | " + kleur.bgRed().yellow().bold(`!!! FAIL: ${this.fail} !!!`) : "");
+		console.log();
+	}
+}
+
+
+module.exports = ModuleChecker;
--- src/index.js
+++ src/index.js
@@ -1,171 +1,411 @@
 /*
- * moleculer-macrometa
- * Copyright (c) 2019 MoleculerJS (https://github.com/moleculerjs/moleculer-macrometa)
+ * moleculer-db-adapter-macrometa
+ * Copyright (c) 2019 MoleculerJS (https://github.com/moleculerjs/moleculer-db-adapter-macrometa)
  * MIT Licensed
  */
 
 "use strict";
 
-const { MoleculerClientError } = require("moleculer").Errors;
+const _ 			= require("lodash");
+const Promise		= require("bluebird");
+const { ServiceSchemaError, MoleculerError } = require("moleculer").Errors;
 
 const Fabric = require("jsc8");
 const c8ql = Fabric.c8ql;
 
-module.exports = {
-
-	name: "macrometa",
+class MacroMetaAdapter {
 
 	/**
-	 * Default settings
+	 * Creates an instance of MacroMetaAdapter.
+	 * 
+	 * @param {Object|String|Array<String>?} opts
+	 *
+	 * @memberof MacroMetaAdapter
 	 */
-	settings: {
-		fabric: {
-			// https://dev.macrometa.io/docs/overview-3#connect-to-c8
-			config: "https://gdn1.macrometa.io",
+	constructor(opts) {
+		if (_.isString(opts) || Array.isArray(opts))
+			this.opts = { url: opts };
+		else 
+			this.opts = opts || {};
+	}
 
-			// https://dev.macrometa.io/docs/overview-3#login
-			email: process.env.FABRIC_EMAIL,
-			password: process.env.FABRIC_PASS,
+	/**
+	 * Initialize adapter
+	 *
+	 * @param {ServiceBroker} broker
+	 * @param {Service} service
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	init(broker, service) {
+		this.broker = broker;
+		this.service = service;
+		this.logger = this.service.logger;
 
-			tenant: null,
-			fabric: null,
-			collection: null
+		const schema = this.service.schema;
+
+		if (!schema.collection) {
+			/* istanbul ignore next */
+			throw new ServiceSchemaError("Missing `collection` definition in schema of service!");
+		}
+	}
+
+	/**
+	 * Connect to database
+	 *
+	 * @returns {Promise}
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	async connect() {
+		this.fabric = new Fabric(this.opts.url);
+
+		await this.login(this.opts.email, this.opts.password);
+
+		this.collection = await this.openCollection(this.service.schema.collection);
+		this.logger.info("Fabric c8 connection has been established.");
+	}
+
+	/**
+	 * Disconnect from database
+	 *
+	 * @returns {Promise}
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	disconnect() {
+		if (this.fabric) {
+			return this.fabric.close();
+		}
+		return Promise.resolve();
+	}
+
+	/**
+	 * Login to the server.
+	 * 
+	 * @param {String} email 
+	 * @param {String} password 
+	 */
+	async login(email, password) {
+		this.logger.info(`Logging in with '${email}'...`);
+		await this.fabric.login(email, password);
+		this.logger.info("Logged in.");
+
+		if (this.opts.tenant) {
+			this.logger.info(`Switch tenant to '${this.opts.tenant}'`);
+			await this.fabric.useTenant(this.opts.tenant);
 		}
 
-	},
+		if (this.opts.fabric) {
+			this.logger.info(`Switch Fabric to '${this.opts.fabric}'`);
+			await this.fabric.usefabric(this.opts.fabric);
+		}
+	}
 
 	/**
-	 * Actions
+	 * Open or create a collection.
+	 * 
+	 * @param {String} name 
+	 * @param {Boolean?} createIfNotExist 
 	 */
-	actions: {
-		/**
-		 * 
-		 */
-		find: {
-			handler(ctx) {
-				return this.findDocuments(ctx, ctx.params);
+	async openCollection(name, createIfNotExist = true) {
+		this.logger.info(`Open '${name}' collection...`);
+		const collection = this.fabric.collection(name);
+		if (!(await collection.exists())) {
+			if (createIfNotExist) {
+				this.logger.info(`Create '${name}' collection...`);
+				await collection.create();
+			} else {
+				throw new MoleculerError(`Collection '${name}' is not exist!`, 500, "COLLECTION_NOT_EXIST", { name });
 			}
-		},
+		}
 
-		/**
-		 * 
-		 */
-		create: {
-			rest: "POST /",
-			handler(ctx) {
-				return this.createDocument(ctx, ctx.params);
-			}
-		},
+		this.logger.info(`Collection '${name}' is opened.`);
 
-
-	},
+		return collection;
+	}
 
 	/**
-	 * Methods
+	 * Find all entities by filters.
+	 *
+	 * Available filter props:
+	 * 	- limit
+	 *  - offset
+	 *  - sort
+	 *  - search
+	 *  - searchFields
+	 *  - query
+	 *
+	 * @param {Object} filters
+	 * @returns {Promise<Array>}
+	 *
+	 * @memberof MacroMetaAdapter
 	 */
-	methods: {
-		/**
-		 * 
-		 * @param {*} email 
-		 * @param {*} password 
-		 */
-		async login(email, password) {
-			this.logger.info(`Logging in with '${email}'...`);
-			await this.fabric.login(email, password);
-			this.logger.info("Logged in.");
+	async find(filters) {
+		const cursor = await this.createCursor(filters);
+		const res = await cursor.all();
+		//cursor.delete(); // no 'await' because we don't want to wait for it.
 
-			if (this.settings.fabric.tenant) {
-				this.logger.info(`Switch tenant to '${this.settings.fabric.tenant}'`);
-				await this.fabric.useTenant(this.settings.fabric.tenant);
+		return res;
+	}
+
+	/**
+	 * Find an entity by query
+	 *
+	 * @param {Object} query
+	 * @returns {Promise}
+	 * @memberof MemoryDbAdapter
+	 */
+	async findOne(query) {
+		const cursor = await this.createCursor({ query });
+		if (cursor.hasNext()) {
+			const res = await cursor.next();
+			//cursor.delete(); // no 'await' because we don't want to wait for it.
+			return res;
+		} else {
+			//cursor.delete(); // no 'await' because we don't want to wait for it.
+			return null;
+		}
+	}
+
+	/**
+	 * Find an entities by key.
+	 *
+	 * @param {String} key
+	 * @returns {Promise<Object>} Return with the found document.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	async findById(key) {
+		return await this.collection.document(key);
+	}
+
+	/**
+	 * Find any entities by IDs.
+	 *
+	 * @param {Array} idList
+	 * @returns {Promise<Array>} Return with the found documents in an Array.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	findByIds(idList) {
+		// TODO: not implemented
+		throw new Error("not implemented");
+		/*
+		return this.collection.find({
+			_id: {
+				$in: idList.map(id => this.stringToObjectID(id))
 			}
+		}).toArray();
+		*/
+	}
 
-			if (this.settings.fabric.fabric) {
-				this.logger.info(`Switch Fabric to '${this.settings.fabric.fabric}'`);
-				await this.fabric.usefabric(this.settings.fabric.fabric);
-			}
-		},
+	/**
+	 * Get count of filtered entites.
+	 *
+	 * Available query props:
+	 *  - search
+	 *  - searchFields
+	 *  - query
+	 *
+	 * @param {Object} [filters={}]
+	 * @returns {Promise<Number>} Return with the count of documents.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	async count(filters) {
+		const cursor = await this.createCursor(filters, { count: true });
+		const res = cursor.count;
+		//cursor.delete(); // no 'await' because we don't want to wait for it.
 
-		/**
-		 * 
-		 * @param {*} name 
-		 * @param {*} createIfNotExist 
-		 */
-		async openCollection(name, createIfNotExist = true) {
-			this.logger.info(`Open '${this.settings.fabric.collection}' collection...`);
-			const collection = this.fabric.collection(name);
-			if (!(await collection.exists())) {
-				if (createIfNotExist) {
-					this.logger.info(`Create '${this.settings.fabric.collection}' collection...`);
-					await collection.create();
-				} else {
-					throw new MoleculerClientError("Collection '${name}' is not exist!", 500, "COLLECTION_NOT_EXIST", { name });
+		return res;
+	}
+
+	/**
+	 * Insert an entity.
+	 *
+	 * @param {Object} entity
+	 * @param {Object?} opts
+	 * @returns {Promise<Object>} Return with the inserted document.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	async insert(entity, opts) {
+		const res = await this.collection.save(entity, Object.assign({ returnNew : true }, opts));
+		return res["new"];
+	}
+
+	/**
+	 * Insert many entities
+	 *
+	 * @param {Array} entities
+	 * @param {Object?} opts
+	 * @returns {Promise<Array<Object>>} Return with the inserted documents in an Array.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	insertMany(entities, opts) {
+		// TODO: is there bulk insert method?
+		return Promise.all(entities.map(entity => this.insert(entity, opts)));
+	}
+
+	/**
+	 * Update many entities by `query` and `update`
+	 *
+	 * @param {Object} query
+	 * @param {Object} update
+	 * @returns {Promise<Number>} Return with the count of modified documents.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	updateMany(query, update) {
+		// TODO: not implemented
+		throw new Error("not implemented");
+	}
+
+	/**
+	 * Update an entity by ID and `update`
+	 *
+	 * @param {String} _id - ObjectID as hexadecimal string.
+	 * @param {Object} update
+	 * @param {Object?} opts
+	 * @returns {Promise<Object>} Return with the updated document.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	updateById(_id, update, opts) {
+		return this.collection.update(_id, update, Object.assign({ returnNew : true }, opts));
+	}
+
+	/**
+	 * Remove entities which are matched by `query`
+	 *
+	 * @param {Object} query
+	 * @returns {Promise<Number>} Return with the count of deleted documents.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	removeMany(query) {
+		// TODO: not implemented
+		throw new Error("not implemented");
+	}
+
+	/**
+	 * Remove an entity by ID.
+	 *
+	 * @param {String} _id
+	 * @param {Object?} opts
+	 * @returns {Promise<Object>} Return with the removed document.
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	async removeById(_id, opts) {
+		return await this.collection.remove(_id, opts);
+	}
+
+	/**
+	 * Clear all entities from collection
+	 *
+	 * @param {Object?} opts
+	 * @returns {Promise}
+	 *
+	 * @memberof MacroMetaAdapter
+	 */
+	clear(opts) {
+		return this.collection.truncate(opts);
+	}
+
+	/**
+	 * Create a filtered cursor.
+	 *
+	 * Available filters in `params`:
+	 *  - search
+	 * 	- sort
+	 * 	- limit
+	 * 	- offset
+	 *  - query
+	 *
+ 	 * @param {Object} params
+ 	 * @param {Object} opts
+	 * @returns {MongoCursor}
+	 */
+	async createCursor(params, opts) {
+		let q;
+		if (params) {
+			// TODO: not implemented
+			throw new Error("not implemented");
+
+			// Full-text search
+			// More info: https://docs.mongodb.com/manual/reference/operator/query/text/
+			if (_.isString(params.search) && params.search !== "") {
+				q = fn.call(this.collection, Object.assign(params.query || {}, {
+					$text: {
+						$search: params.search
+					}
+				}));
+
+				if (q.project && !isCounting)
+					q.project({ _score: { $meta: "textScore" } });
+
+				if (q.sort && !isCounting) {
+					q.sort({
+						_score: {
+							$meta: "textScore"
+						}
+					});
+				}
+			} else {
+				q = fn.call(this.collection, params.query);
+
+				// Sort
+				if (params.sort && q.sort) {
+					let sort = this.transformSort(params.sort);
+					if (sort)
+						q.sort(sort);
 				}
 			}
 
-			this.logger.info(`Collection '${this.settings.fabric.collection}' is opened.`);
+			// Offset
+			if (_.isNumber(params.offset) && params.offset > 0)
+				q.skip(params.offset);
 
-			return collection;
-		},
+			// Limit
+			if (_.isNumber(params.limit) && params.limit > 0)
+				q.limit(params.limit);
 
-		/**
-		 *
-		 * More info: https://dev.macrometa.io/docs/documentcollection#documentcollectionsave
-		 * 
-		 * @param {*} ctx
-		 * @param {*} params
-		 * @returns
-		 */
-		async createDocument(ctx, params) {
-			return await this.collection.save(params);
-		},
-
-		/**
-		 *
-		 * More info: https://dev.macrometa.io/docs/queries-1#queries
-		 * 
-		 * @param {*} ctx
-		 * @param {*} params
-		 */
-		async findDocuments(ctx, params) {
-			const cursor = await this.fabric.query(`FOR row IN ${this.collection.name} RETURN row`);
-			const result = await cursor.all();
-
-			return result;
-		},
-
-		async getDocument(ctx, key) {
-			try {
-				const doc = await this.collection.document(key);
-				return doc;
-			} catch(err) {
-				throw new MoleculerClientError("Document is not found", 404, "NO_DOCUMENT", { key });
-			}
-		}
-	},
-
-	/**
-	 * Service created lifecycle event handler
-	 */
-	created() {
-		this.fabric = new Fabric(this.settings.fabric.config);
-	},
-
-	/**
-	 * Service started lifecycle event handler
-	 */
-	async started() {
-		await this.login(this.settings.fabric.email, this.settings.fabric.password);
-
-		if (this.settings.fabric.collection) {
-			this.collection = await this.openCollection(this.settings.fabric.collection);
+			return q;
 		}
 
-	},
-
-	/**
-	 * Service stopped lifecycle event handler
-	 */
-	stopped() {
-
+		// If not params
+		return await this.fabric.query(`FOR row IN ${this.collection.name} RETURN row`, {}, opts);
 	}
-};
+
+	/**
+	 * Convert the `sort` param to a `sort` object to Mongo queries.
+	 *
+	 * @param {String|Array<String>|Object} paramSort
+	 * @returns {Object} Return with a sort object like `{ "votes": 1, "title": -1 }`
+	 * @memberof MacroMetaAdapter
+	 */
+	transformSort(paramSort) {
+		let sort = paramSort;
+		if (_.isString(sort))
+			sort = sort.replace(/,/, " ").split(" ");
+
+		if (Array.isArray(sort)) {
+			let sortObj = {};
+			sort.forEach(s => {
+				if (s.startsWith("-"))
+					sortObj[s.slice(1)] = -1;
+				else
+					sortObj[s] = 1;
+			});
+			return sortObj;
+		}
+
+		return sort;
+	}
+
+}
+
+module.exports = MacroMetaAdapter;
